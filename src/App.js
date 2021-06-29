33263cda16fe09a0e7f5cf2091146df95d488117
the-halfbloodprince <ammanishkumardas@gmail.com>
2021-06-29 21:23:46 +0530

    Just a minor demo commit to explain git 2

--- src/App.js
+++ src/App.js
@@ -4,158 +4,158 @@ import Group from './components/group'
 import './App.css'
 
 function App() {
-  
-  const groups = [
-    'Not Started',
-    'In Progress',
-    'Completed'
-  ]
 
-  const tasks = [
-      {
-          name: 'Code',
-          group: 'In Progress'
-      },
-      {
-          name: 'Watch Shakira',
-          group: 'Completed'
-      },
-      {
-          name: 'Code More',
-          group: 'Not Started'
-      },
-      {
-          name: 'Sleep',
-          group: 'Not Started'
-      },
-      {
-          name: 'Scream for no reason',
-          group: 'Completed'
-      },
-      {
-          name: 'Stop',
-          group: 'Not Started'
-      },
-      {
-          name: 'Jump from balcony',
-          group: 'In Progress'
-      },
-      {
-          name: 'Listen to music',
-          group: 'In Progress'
-      },
-      {
-          name: 'Code',
-          group: 'In Progress'
-      },
-      {
-          name: 'Watch Shakira',
-          group: 'Completed'
-      },
-      {
-          name: 'Code More',
-          group: 'Not Started'
-      },
-      {
-          name: 'Sleep',
-          group: 'Not Started'
-      },
-      {
-          name: 'Scream for no reason',
-          group: 'Completed'
-      },
-      {
-          name: 'Stop',
-          group: 'Not Started'
-      },
-      {
-          name: 'Jump from balcony',
-          group: 'In Progress'
-      },
-      {
-          name: 'Listen to music',
-          group: 'In Progress'
-      },      
-      {
-          name: 'Code',
-          group: 'In Progress'
-      },
-      {
-          name: 'Watch Shakira',
-          group: 'Completed'
-      },
-      {
-          name: 'Code More',
-          group: 'Not Started'
-      },
-      {
-          name: 'Sleep',
-          group: 'Not Started'
-      },
-      {
-          name: 'Scream for no reason',
-          group: 'Completed'
-      },
-      {
-          name: 'Stop',
-          group: 'Not Started'
-      },
-      {
-          name: 'Jump from balcony',
-          group: 'In Progress'
-      },
-      {
-          name: 'Listen to music',
-          group: 'In Progress'
-      },
-      {
-          name: 'Code',
-          group: 'In Progress'
-      },
-      {
-          name: 'Watch Shakira',
-          group: 'Completed'
-      },
-      {
-          name: 'Code More',
-          group: 'Not Started'
-      },
-      {
-          name: 'Sleep',
-          group: 'Not Started'
-      },
-      {
-          name: 'Scream for no reason',
-          group: 'Completed'
-      },
-      {
-          name: 'Stop',
-          group: 'Not Started'
-      },
-      {
-          name: 'Jump from balcony',
-          group: 'In Progress'
-      },
-      {
-          name: 'Listen to music',
-          group: 'In Progress'
-      },      
-  ]
+    const groups = [
+        'Not Started',
+        'In Progress',
+        'Completed'
+    ]
 
-  return (
-    <div className="App">
-        <Header name="Lokesh" fanOf="shakira" />
-        <div className="groups">
-          {
-            // (a, b) => (a+b)
-            //map function takes each element of an array or object and returns something from it
-            groups.map((group) => (
-                    <Group name={group} tasks={tasks.filter( task => task.group === group )} />
-            ))
-          }
+    const tasks = [
+        {
+            name: 'Code',
+            group: 'In Progress'
+        },
+        {
+            name: 'Watch Shakira',
+            group: 'Completed'
+        },
+        {
+            name: 'Code More',
+            group: 'Not Started'
+        },
+        {
+            name: 'Sleep',
+            group: 'Not Started'
+        },
+        {
+            name: 'Scream for no reason',
+            group: 'Completed'
+        },
+        {
+            name: 'Stop!!!',
+            group: 'Not Started'
+        },
+        {
+            name: 'Jump from balcony',
+            group: 'In Progress'
+        },
+        {
+            name: 'Listen to music',
+            group: 'In Progress'
+        },
+        {
+            name: 'Code',
+            group: 'In Progress'
+        },
+        {
+            name: 'Watch Shakira',
+            group: 'Completed'
+        },
+        {
+            name: 'Code More',
+            group: 'Not Started'
+        },
+        {
+            name: 'Sleep',
+            group: 'Not Started'
+        },
+        {
+            name: 'Scream for no reason',
+            group: 'Completed'
+        },
+        {
+            name: 'Stop',
+            group: 'Not Started'
+        },
+        {
+            name: 'Jump from balcony',
+            group: 'In Progress'
+        },
+        {
+            name: 'Listen to music',
+            group: 'In Progress'
+        },
+        {
+            name: 'Code',
+            group: 'In Progress'
+        },
+        {
+            name: 'Watch Shakira',
+            group: 'Completed'
+        },
+        {
+            name: 'Code More',
+            group: 'Not Started'
+        },
+        {
+            name: 'Sleep',
+            group: 'Not Started'
+        },
+        {
+            name: 'Scream for no reason',
+            group: 'Completed'
+        },
+        {
+            name: 'Stop',
+            group: 'Not Started'
+        },
+        {
+            name: 'Jump from balcony',
+            group: 'In Progress'
+        },
+        {
+            name: 'Listen to music',
+            group: 'In Progress'
+        },
+        {
+            name: 'Code',
+            group: 'In Progress'
+        },
+        {
+            name: 'Watch Shakira',
+            group: 'Completed'
+        },
+        {
+            name: 'Code More',
+            group: 'Not Started'
+        },
+        {
+            name: 'Sleep',
+            group: 'Not Started'
+        },
+        {
+            name: 'Scream for no reason',
+            group: 'Completed'
+        },
+        {
+            name: 'Stop',
+            group: 'Not Started'
+        },
+        {
+            name: 'Jump from balcony',
+            group: 'In Progress'
+        },
+        {
+            name: 'Listen to music',
+            group: 'In Progress'
+        },
+    ]
+
+    return (
+        <div className="App">
+            <Header name="Lokesh" fanOf="shakira" />
+            <div className="groups">
+                {
+                    // (a, b) => (a+b)
+                    //map function takes each element of an array or object and returns something from it
+                    groups.map((group) => (
+                        <Group name={group} tasks={tasks.filter(task => task.group === group)} />
+                    ))
+                }
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default App
